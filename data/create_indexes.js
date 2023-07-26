a3cad97302ac83ab39828591d2d9a4b188e15f78
northCoder <northcoder.root@gmail.com>
2023-07-26 17:49:41 -0400

    Streamline index creation script.

--- data/create_indexes.js
+++ data/create_indexes.js
@@ -1,30 +1,34 @@
 //
 // Create an extra index on the main and test collections
 //
-let qdb = connect( 'mongodb://localhost:27017/stackoverflow' );
-let questions = qdb.getCollection("questions");
-questions.createIndex (
-  {
-    "question_id": 1, 
-    "site": 1
-  },
-  {
-    unique: true,
-    sparse: true,
-    name: "qu_and_site"
-  }
-);
 
-let testdb = connect( 'mongodb://localhost:27017/test' );
-let test_questions = testdb.getCollection("test_questions");
-test_questions.createIndex (
+let url = "mongodb://localhost:27017/";
+
+let connections = [
   {
-    "question_id": 1, 
-    "site": 1
+    "dbName": "stackoverflow",
+    "collectionName": "questions"
   },
   {
-    unique: true,
-    sparse: true,
-    name: "qu_and_site"
+    "dbName": "test",
+    "collectionName": "test_questions"
   }
-);
+];
+
+connections.forEach(conn => createIndex(conn));
+
+function createIndex(conn) {
+  let db = connect(url + conn.dbName);
+  let collection = db.getCollection(conn.collectionName);
+  collection.createIndex (
+    {
+      "question_id": 1, 
+      "site": 1
+    },
+    {
+      unique: true,
+      sparse: true,
+      name: "qu_and_site"
+    }
+  );
+}
